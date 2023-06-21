735e4ee4c06c15379e9676e0fa829bd79d363fe0
Lorenz <darshan310268@gmail.com>
2023-06-21 20:47:15 +0530

    tests

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,31 +1,74 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { Debugger } = require('../dist');
-
-const { token } = require('./config');
-
-const client = new Client({
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var discord_js_1 = require("discord.js");
+var dist_1 = require("../dist");
+var token = require('./config').token;
+var client = new discord_js_1.Client({
     intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent
+        discord_js_1.GatewayIntentBits.Guilds,
+        discord_js_1.GatewayIntentBits.GuildMessages,
+        discord_js_1.GatewayIntentBits.GuildMembers,
+        discord_js_1.GatewayIntentBits.MessageContent
     ]
 });
-
-const debug = new Debugger(client, {
+var debug = new dist_1.Debugger(client, {
     secrets: [process.env.TEST]
     // owners: ['838285943365435453']
 });
-
-client.on('ready', () => console.log(`Logged in as ${client.user.tag}`));
-
-client.on('messageCreate', async (message) => {
-    if (message.content === '!ping') {
-        return message.reply('pong!');
-    } else if (message.content.startsWith('!debug')) {
-        const args = message.content.split(' ').slice(1);
-        await debug.run(message, args);
-    }
-});
-
+client.on('ready', function () { return console.log("Logged in as ".concat(client.user.tag)); });
+client.on('messageCreate', function (message) { return __awaiter(void 0, void 0, void 0, function () {
+    var args;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!(message.content === '!ping')) return [3 /*break*/, 1];
+                message.reply('pong!');
+                return [3 /*break*/, 3];
+            case 1:
+                if (!message.content.startsWith('!debug')) return [3 /*break*/, 3];
+                args = message.content.split(' ').slice(1);
+                return [4 /*yield*/, debug.run(message, args)];
+            case 2:
+                _a.sent();
+                _a.label = 3;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
 client.login(token);
